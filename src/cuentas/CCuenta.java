995e49f96fd01b9c6e4ff0ca68057609a16b74b3
diego <diego@Diego-PC>
2022-03-15 20:27:17 +0100

    Commit creando JavaDoc parte 1

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -2,113 +2,176 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cuentas;
 
-
+/**
+ * Clase CCuenta que gestiona los métodos del objeto
+ *
+ * @author diego
+ */
 public class CCuenta {
 
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
-    
+
     /**
-     * @return the cuenta
+     * Método getter para nombre
+     *
+     * @return nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Método setter para nombre
+     *
+     * @param nombre nombre
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Método getter para cuenta
+     *
+     * @return cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Método setter para cuenta
+     *
+     * @param cuenta cuenta
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Método getter para saldo
+     *
+     * @return saldo
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Método setter para saldo
+     *
+     * @param saldo saldo
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
-    public CCuenta()
-    {
-    }
-
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
-    }
-
-    public void asignarNombre(String nom)
-    {
-        setNombre(nom);
-    }
-
-    public String obtenerNombre()
-    {
-        return getNombre();
-    }
-
-
-    public double estado()
-    {
-        return getSaldo();
-    }
-
-
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
-            throw new Exception("No se puede ingresar una cantidad negativa");
-        setSaldo(getSaldo() + cantidad);
-    }
-
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
-        setSaldo(getSaldo() - cantidad);
-    }
-
-
-    public String obtenerCuenta()
-    {
-        return getCuenta();
-    }
-
-
-    public String getNombre() {
-        return nombre;
-    }
-
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-
+    /**
+     * Método getter para Tipo de Interés
+     *
+     * @return tipo de interés
+     */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
-
+    /**
+     * Método setter para tipo de interés
+     *
+     * @param tipoInterés Tipo de interés
+     */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
-}
 
+    /**
+     * Constructor por defecto para CCuenta
+     */
+    public CCuenta() {
+    }
+
+    /**
+     * Constructor parametrizado para CCuenta
+     *
+     * @param nom nombre
+     * @param cue cuenta
+     * @param sal saldo
+     * @param tipo tipo
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
+    }
+
+    /**
+     * Método para asignar un nombre
+     *
+     * @param nom nombre
+     */
+    public void asignarNombre(String nom) {
+        setNombre(nom);
+    }
+
+    /**
+     * Método para obtener un nombre
+     *
+     * @return nombre
+     */
+    public String obtenerNombre() {
+        return getNombre();
+    }
+
+    /**
+     * Método para obtener un saldo
+     *
+     * @return Saldo
+     */
+    public double estado() {
+        return getSaldo();
+    }
+
+    /**
+     * Método para ingresar una cantidad
+     *
+     * @param cantidad Cantidad a ingresar
+     * @throws Exception Excepción controlada para cantidades negativas
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        }
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    /**
+     * Método para retirar una cantidad
+     *
+     * @param cantidad Cantidad a retirar
+     * @throws Exception Excepciones controladas para retirar cantidades
+     * negativas o si no hay suficiente saldo
+     */
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
+        setSaldo(getSaldo() - cantidad);
+    }
+
+    /**
+     * Método para obtener cuenta
+     *
+     * @return Cuenta
+     */
+    public String obtenerCuenta() {
+        return getCuenta();
+    }
+
+}
--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -3,10 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 package cuentas;
-
 
 public class Main {
 
@@ -14,16 +11,16 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
         operativa_cuenta(cuenta1);
     }
 
     private static void operativa_cuenta(CCuenta cuenta1) {
         double saldoActual;
         float cantidad;
-        
+
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es" + saldoActual);
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
@@ -37,4 +34,3 @@ public class Main {
         }
     }
 }
-
